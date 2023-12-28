930f2f7317b580e11215fda0354608000cfc2eaf
calebosam <caleb.osam@amalitech.com>
2023-12-28 14:12:28 +0000

    feat: create endpoint with redis caching

--- app.js
+++ app.js
@@ -4,12 +4,36 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const User = require("./models/UserModel");
 const AppError = require("./utils/AppError");
+const Redis = require('redis');
+const axios = require("axios")
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
+
+const redisClient = Redis.createClient({
+  url: process.env.REDIS_URL
+})
+
+redisClient.connect().catch(console.error);
+
+app.delete("/api/v1")
+app.get("/api/v1/externalUsers", async(req, res, next)=>{
+  const data = await redisClient.get("externalUsers")
+  if(data === null) {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/users`
+    );
+    const externalUsers = JSON.stringify(data);
+    await redisClient.set("externalUsers", externalUsers);
+    res.json({ src: "typicode", externalUsers: data });
+  }else{
+    const parsedData = JSON.parse(data)
+    res.status(200).json({status: "success", src: "redis", data: parsedData})
+  }
+})
 app.get("/api/v1/users", async (req, res, next) => {
   const users = await User.find();
   res.status(200).json({ status: "success", data: users });
